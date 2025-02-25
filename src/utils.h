1b27e9be6c820205e345606aa30278686d8e87e3
Fries Carton <friescarton@gmail.com>
2025-02-25 14:46:28 +0100

    Integrate our logging into raylib

--- src/utils.h
+++ src/utils.h
@@ -27,16 +27,19 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <Core/Log.h>
+
 #if defined(PLATFORM_ANDROID)
     #include <stdio.h>                      // Required for: FILE
     #include <android/asset_manager.h>      // Required for: AAssetManager
 #endif
 
 #if defined(SUPPORT_TRACELOG)
-    #define TRACELOG(level, ...) TraceLog(level, __VA_ARGS__)
+    #define TRACELOG(level, ...) PrintLog(level-1, __FILE__, __LINE__, __VA_ARGS__)
+    //TraceLog(level, __VA_ARGS__)
 
     #if defined(SUPPORT_TRACELOG_DEBUG)
-        #define TRACELOGD(...) TraceLog(LOG_DEBUG, __VA_ARGS__)
+        #define TRACELOGD(...) LogDebug(__VA_ARGS__)
     #else
         #define TRACELOGD(...) (void)0
     #endif
